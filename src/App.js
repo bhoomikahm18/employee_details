ce9bfbf98e16471672a6a0c3ac85ef413d3ee8d1
Bhoomika <bhoomikahm18@gmail.com>
2024-06-21 18:51:00 +0530

    Removing unwanted files

--- src/App.js
+++ src/App.js
@@ -1,14 +1,11 @@
 import React from "react";
-import EmployeeList from "./components/EmployeeList.jsx";
-import AddEmployee from "./components/AddEmployee.jsx";
 import { Route, Router, Routes } from "react-router-dom";
 
 function App() {
   return (
     <Router>
         <Routes>
-          <Route path="/" element={<EmployeeList />} />
-          <Route path="/add" element={<AddEmployee />} />
+          
         </Routes>
     </Router>
   );
--- src/components/EmployeeList.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-function EmployeeList() {
-    return (
-        <div>EmployeeList</div>
-    )
-}
-
-export default EmployeeList
